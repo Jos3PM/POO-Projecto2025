32cb0b4eaadf0f1c7767d55f58253b2ba94d94b9
Jos3PM <just76111@hotmail.com>
2025-05-10 17:23:46 -0500

    Add files via upload

---
+++ CrudF/src/test/java/pe/edu/upeu/crudf/CrudFApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upeu.crudf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudFApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

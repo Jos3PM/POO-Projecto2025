2316182498872ddc91c61129a72c56df98e26579
Jos3PM <just76111@hotmail.com>
2025-06-07 18:37:21 -0500

    Deleted pom.xml

--- CrudH/src/main/java/pe/edu/upeu/crudh/repository/PacienteRepository.java
+++
@@ -1,7 +0,0 @@
-package pe.edu.upeu.crudh.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pe.edu.upeu.crudh.model.Paciente;
-
-public interface PacienteRepository extends JpaRepository<Paciente, Long> {
-}

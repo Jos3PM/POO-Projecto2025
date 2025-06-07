049b93eb310c89108f9359afe263ced26f21b1dc
Jos3PM <just76111@hotmail.com>
2025-06-07 18:37:09 -0500

    Deleted patient.fxml

--- src/main/java/pe/edu/upeu/crudf/repository/HealthStatusRepository.java
+++
@@ -1,7 +0,0 @@
-package pe.edu.upeu.crudf.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pe.edu.upeu.crudf.model.HealthStatus;
-
-public interface HealthStatusRepository extends JpaRepository<HealthStatus, Long> {
-}
--- src/main/java/pe/edu/upeu/crudf/repository/MedicalHistoryRepository.java
+++
@@ -1,7 +0,0 @@
-package pe.edu.upeu.crudf.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pe.edu.upeu.crudf.model.MedicalHistory;
-
-public interface MedicalHistoryRepository extends JpaRepository<MedicalHistory, Long> {
-}

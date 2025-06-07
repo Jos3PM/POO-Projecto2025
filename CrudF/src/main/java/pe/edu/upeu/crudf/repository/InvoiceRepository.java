10a9e3fbab806fc2a9ba10f2af2694e9570953c9
Jos3PM <just76111@hotmail.com>
2025-06-07 18:37:17 -0500

    Deleted mvnw.cmd

--- CrudF/src/main/java/pe/edu/upeu/crudf/repository/InvoiceRepository.java
+++
@@ -1,7 +0,0 @@
-package pe.edu.upeu.crudf.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pe.edu.upeu.crudf.model.Invoice;
-
-public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
-}
--- CrudF/src/main/java/pe/edu/upeu/crudf/repository/PatientRepository.java
+++
@@ -1,7 +0,0 @@
-package pe.edu.upeu.crudf.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pe.edu.upeu.crudf.model.Patient;
-
-public interface PatientRepository extends JpaRepository<Patient, Long> {
-}
